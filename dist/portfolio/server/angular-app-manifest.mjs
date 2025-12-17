5d6145bbea947ba0de53654e3badb6784b2e3d06
Jojo94UTD <94547339+Jojo94UTD@users.noreply.github.com>
2025-12-17 20:12:40 +0100

    nn

--- dist/portfolio/server/angular-app-manifest.mjs
+++ dist/portfolio/server/angular-app-manifest.mjs
@@ -7,7 +7,6 @@ export default {
   routes: [
   {
     "renderMode": 2,
-    "redirectTo": "/accueil",
     "route": "/"
   },
   {
@@ -20,16 +19,17 @@ export default {
   },
   {
     "renderMode": 2,
-    "redirectTo": "/accueil",
+    "redirectTo": "/",
     "route": "/**"
   }
 ],
   entryPointToBrowserMapping: undefined,
   assets: {
-    'index.csr.html': {size: 9442, hash: 'f13fd81db4f588c589096a39414f73c6e1f951081deb8bf5b1f366a2c0e08387', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
-    'index.server.html': {size: 9798, hash: 'ba87d650449895eea6b4159124d39ec09c7aed06b8f6213f95fd5cc58e7ab3b8', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
-    'enedis/index.html': {size: 16618, hash: '3e145265d446b935dfd484037eeebddbedbcaa2736c4aaac04073bd9c180f681', text: () => import('./assets-chunks/enedis_index_html.mjs').then(m => m.default)},
-    'accueil/index.html': {size: 21736, hash: '522cffadb0f2b5d7c7077a9ae282a3e1c72c46e73463087168c0822a369189d7', text: () => import('./assets-chunks/accueil_index_html.mjs').then(m => m.default)},
+    'index.csr.html': {size: 9442, hash: 'f20814329c40837391659c2adeea29bde5630cdfd01d7bdd6aa79b66b7c49219', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
+    'index.server.html': {size: 9798, hash: '9b83d1208b48348c61225a7e6bcea9a5f5c315ed49cc9a7be16fa02ff87df94d', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
+    'index.html': {size: 21736, hash: 'd0dfe115b16a8a9b56e228db9aa8ad3c3ec1fe6f02804cae8a525ab3240ba3f8', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
+    'accueil/index.html': {size: 21736, hash: 'd0dfe115b16a8a9b56e228db9aa8ad3c3ec1fe6f02804cae8a525ab3240ba3f8', text: () => import('./assets-chunks/accueil_index_html.mjs').then(m => m.default)},
+    'enedis/index.html': {size: 16618, hash: '543233b055f66e937cd254de71cf117b4b2f71d87ba909a385900774bfc822c6', text: () => import('./assets-chunks/enedis_index_html.mjs').then(m => m.default)},
     'styles-3M5NMCC2.css': {size: 32, hash: 'UDqEmAbNj70', text: () => import('./assets-chunks/styles-3M5NMCC2_css.mjs').then(m => m.default)}
   },
 };
